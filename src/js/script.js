08d7dce26229b26fa3f0a899c28a5b3915a3403f
Владимир Королев <vladimirkorolev@MacBook-Air-Vladimir.local>
2024-11-16 08:22:10 +0300

    second

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,13 @@
-
-
 function init() {
-    let map = new ymaps.Map('map-test', {
-        center: [55.751868057224556,37.61887408245741],
-        zoom: 10    
-    })
+  let map = new ymaps.Map("map-test", {
+    center: [55.751868057224556, 37.61887408245741],
+    zoom: 10,
+  });
+  let placemark = new ymaps.Placemark(
+    [55.751868057224556, 37.61887408245741],
+    {},
+    {}
+  );
+  map.geoObjects.add(placemark);
 }
 ymaps.ready(init);
